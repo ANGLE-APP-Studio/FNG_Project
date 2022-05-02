d15035d03659b952e721b08fc5f074286f1a7207
igh2932 <76648508+igh2932@users.noreply.github.com>
2022-05-02 23:30:19 +0900

    LGN-002 회원가입,로그인 기능 구현 사용자가 회원가입시 등록한 이메일 , 비밀번호로 로그인 이 가능하게 했음 (아이디로 로그인 하게 하고싶었지만 파베에서 이메일 로그인 지원이기 때문에 하는수 없이 이메일로 받게함 DB수정 해야함) 구글 로그인 기능 구현 아직안함 일단 이메일 로그인 기능 구현 먼저했음

--- app/src/main/java/com/example/fangle/account/sign_in/SignInActivity.java
+++ app/src/main/java/com/example/fangle/account/sign_in/SignInActivity.java
@@ -1,19 +1,35 @@
 package com.example.fangle.account.sign_in;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.fangle.MainActivity;
 import com.example.fangle.R;
+import com.example.fangle.account.sign_up.SignUpActivity;
+import com.example.fangle.intro.IntroActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignInActivity extends AppCompatActivity {
 
+    // 파이어베이스 로그인 관련 선언
+    private FirebaseAuth firebaseAuth;
+
+    // 레이아웃 관련 선언
     EditText sign_in_id,sign_in_password;
     Button sign_in_button;
 
+    // 사용자가 입력한 로그인 정보 값 선언
+    String id,password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +40,39 @@ public class SignInActivity extends AppCompatActivity {
 
         sign_in_button = (Button) findViewById(R.id.sign_in_button);
 
+        // 파이어베이스 로그인
+        firebaseAuth = FirebaseAuth.getInstance();
+
         sign_in_button.setOnClickListener(new View.OnClickListener() {
             // 회원정보 가 맞는지 확인하고 맞으면 메인화면 아니면 정보를 다시 확인 하라는 에러 표시
             @Override
-            public void onClick(View view) {
-
+            public void onClick(View v) {
+                get_sign_in_data();
+                sign_in();
             }
         });
     }
+
+    // 사용자가 입력한 로그인정보를 받는다.
+    public void get_sign_in_data(){
+        id = sign_in_id.getText().toString().trim();
+        password = sign_in_password.getText().toString().trim();
+    }
+
+    // 파이어베이스 로그인
+    private void sign_in(){
+        firebaseAuth.signInWithEmailAndPassword(id,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Intent intent = new Intent(SignInActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(SignInActivity.this,"로그인 오류",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/example/fangle/account/sign_up/SignUpActivity.java
+++ app/src/main/java/com/example/fangle/account/sign_up/SignUpActivity.java
@@ -1,10 +1,13 @@
 package com.example.fangle.account.sign_up;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -14,12 +17,20 @@ import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.example.fangle.R;
+import com.example.fangle.intro.IntroActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+
+
 public class SignUpActivity extends AppCompatActivity {
-    //생년월일 입력관련 선언
+    // 생년월일 입력관련 선언
     SimpleDateFormat mFormat_year = new SimpleDateFormat("yyyy");
     SimpleDateFormat mFormat_month = new SimpleDateFormat("MM");
     SimpleDateFormat mFormat_day = new SimpleDateFormat("dd");
@@ -30,7 +41,11 @@ public class SignUpActivity extends AppCompatActivity {
     private int year = Integer.parseInt(mFormat_year.format(date));
     private int month = Integer.parseInt(mFormat_month.format(date));
     private int day = Integer.parseInt(mFormat_day.format(date));
-    
+
+    // 파이어베이스 회원가입 관련 선언
+    private static final String TAG = "SignUpActivity";
+    private FirebaseAuth mAuth;
+
     // 레이아웃 관련 선언
     EditText sign_up_id,sign_up_password,sign_up_email,sign_up_phone_number,sign_up_nickname,sign_up_birthdate,sign_up_gender;
     Button birthdate_button,sign_up_button;
@@ -42,6 +57,9 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        // 파이어베이스 mAuth
+        mAuth = FirebaseAuth.getInstance();
 
         sign_up_id = (EditText) findViewById(R.id.sign_up_id);
         sign_up_password = (EditText) findViewById(R.id.sign_up_password);
@@ -65,12 +83,13 @@ public class SignUpActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 get_data();
+                sign_Up();
             }
         });
 
     }
 
-    // 입력한 값을 저장한다.
+    // 입력한 회원정보 값을 저장한다.
     // 이 값을 데이터 베이스로 넘겨야함
     public void get_data(){
         id = sign_up_id.getText().toString(); // 사용자 아이디
@@ -82,7 +101,43 @@ public class SignUpActivity extends AppCompatActivity {
         gender = sign_up_gender.getText().toString(); // 사용자 성별
     }
 
-    //날짜 설정
+    // When initializing your Activity, check to see if the user is currently signed in.
+    // 활동을 초기화할 때 사용자가 현재 로그인되어 있는지 확인하십시오.
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        //사용자가 로그인했는지(null이 아님) 확인하고 그에 따라 UI를 업데이트합니다.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+
+    // 파이어베이스 회원가입
+    private void sign_Up(){
+        mAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    // Sign in success, update UI with the signed-in user's information
+                    // 로그인 성공, 로그인한 사용자 정보로 UI 업데이트
+                    Log.d(TAG, "createUserWithEmail:success");
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    Intent intent = new Intent(SignUpActivity.this, IntroActivity.class);
+                    startActivity(intent);
+                    Toast.makeText(SignUpActivity.this, "등록 완료", Toast.LENGTH_SHORT).show();
+                    finish();
+                }else {
+                    // If sign in fails, display a message to the user.
+                    // 로그인에 실패하면 사용자에게 메시지를 표시합니다.
+                    Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                    Toast.makeText(SignUpActivity.this, "등록 에러", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            }
+        });
+    }
+
+    //생년월일 설정
     private void showDateTimePicker(TextView date) {
 
         DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
