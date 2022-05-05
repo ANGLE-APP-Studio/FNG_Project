937f337c56ada798741d5bdbe8cf60b6848d7d0f
igh2932 <76648508+igh2932@users.noreply.github.com>
2022-05-05 14:54:55 +0900

    커밋 다시함

---
+++ app/src/main/java/com/example/fangle/main/community.java
@@ -0,0 +1,23 @@
+package com.example.fangle.main;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.fangle.R;
+
+public class community extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_community);
+    }
+
+    public void join01Click(View view){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("");
+    }
+}
