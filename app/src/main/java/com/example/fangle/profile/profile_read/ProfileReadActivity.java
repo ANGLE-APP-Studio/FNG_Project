87424bf17f73f30e9b216c0e21f04377d17411a1
igh2932 <76648508+igh2932@users.noreply.github.com>
2022-05-07 18:36:13 +0900

    PFR-001 프로필 레이아웃 기본 구현

---
+++ app/src/main/java/com/example/fangle/profile/profile_read/ProfileReadActivity.java
@@ -0,0 +1,14 @@
+package com.example.fangle.profile.profile_read;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.fangle.R;
+import android.os.Bundle;
+
+public class ProfileReadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_read);
+    }
+}
