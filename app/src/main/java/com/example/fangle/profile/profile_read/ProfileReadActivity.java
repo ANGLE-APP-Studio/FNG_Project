ce34e3fc84cd252cf0cfeedabd72b527578ee7b6
igh2932 <76648508+igh2932@users.noreply.github.com>
2022-05-09 01:08:43 +0900

    PFR-001 기능 베이스 구현

--- app/src/main/java/com/example/fangle/profile/profile_read/ProfileReadActivity.java
+++ app/src/main/java/com/example/fangle/profile/profile_read/ProfileReadActivity.java
@@ -3,12 +3,54 @@ package com.example.fangle.profile.profile_read;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.fangle.R;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ProfileReadActivity extends AppCompatActivity {
+    // 레이아웃 선언
+    Button Log_out_button,nft_View_more_button,member_secession_button,member_modify_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_read);
+
+        Log_out_button = (Button) findViewById(R.id.Log_out_button);
+        nft_View_more_button = (Button) findViewById(R.id.nft_View_more_button);
+        member_secession_button = (Button) findViewById(R.id.member_secession_button);
+        member_modify_button = (Button) findViewById(R.id.member_modify_button);
+
+        // 로그아웃 기능 구현
+        Log_out_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        // nft 더보기 기능구현
+        nft_View_more_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // nft 컬렉션으로 연결 시켜라
+            }
+        });
+
+        // 회원 탈퇴 기능 구현
+        member_secession_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        // 회원 수정 기능 구현
+        member_modify_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
