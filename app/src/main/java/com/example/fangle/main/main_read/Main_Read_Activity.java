38300298c5ee30b3684b03a7a5a3e9dc1fbeafe6
igh2932 <igh2932@gmail.com>
2022-05-10 15:41:34 +0900

    FNG-001 메인화면 레이아웃

---
+++ app/src/main/java/com/example/fangle/main/main_read/Main_Read_Activity.java
@@ -0,0 +1,16 @@
+package com.example.fangle.main.main_read;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.fangle.R;
+
+public class Main_Read_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_read);
+    }
+}
