8e9205375714e6253e41dd27fd396650cc624a89
buyong lee <dqd001@naver.com>
2022-05-11 02:04:44 +0900

    CNR_001 / NFT 컬렉션 구축 목적으로 VSC IDE 환경의 솔리디티 기본 파일 및 안드로이드 구동을 위한 flutter 포함 폴더입니다. NFT 구축상 안드로이드 스튜디오 자체에서 구축이 불가능해 일단 VSC로 폴더를 따로 만들고 이후 혹시 모를 프론트 구동을 위해 flutter 파일을 같이 포함했습니다. 양이 많아도 이해 바랍니다

---
+++ CNR_001/contracts/MintJYPToken.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identfier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+
+contract MintJYPToken is ERC721Enumerable {
+    constructor() ERC721("h662JYP", "HAS") {}
+
+        function mintJYPToken() public {    //
+            uint256 JYPTokenId = totalSupply() +1;
+            _mint(msg.sender, JYPTokenId);
+        }
+
+}
