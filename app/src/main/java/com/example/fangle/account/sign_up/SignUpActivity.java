53a8f09dd7c87c1645fee0cc8985caebbc76638c
igh2932 <76648508+igh2932@users.noreply.github.com>
2022-05-12 00:36:40 +0900

    LGN-004 jsp , Maria DB연동 시도

--- app/src/main/java/com/example/fangle/account/sign_up/SignUpActivity.java
+++ app/src/main/java/com/example/fangle/account/sign_up/SignUpActivity.java
@@ -3,12 +3,13 @@ package com.example.fangle.account.sign_up;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask; //
+import android.os.Bundle;
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -26,8 +27,17 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.io.IOException;//
+import java.io.BufferedReader;//
+import java.io.InputStreamReader;//
+import java.io.OutputStreamWriter;//
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import java.net.HttpURLConnection;//
+import java.net.MalformedURLException;//
+import java.net.URL; //
 
 
 
@@ -60,6 +70,10 @@ public class SignUpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        // jsp
+        CustomTask task = new CustomTask();
+        task.execute("rain483","1234");
+
         // 파이어베이스 mAuth
         mAuth = FirebaseAuth.getInstance();
 
@@ -83,6 +97,47 @@ public class SignUpActivity extends AppCompatActivity {
 
 
 
+    }
+    class CustomTask extends AsyncTask<String, Void, String> {
+        String sendMsg, receiveMsg;
+        @Override
+        // doInBackground의 매개값이 문자열 배열인데요. 보낼 값이 여러개일 경우를 위해 배열로 합니다.
+        protected String doInBackground(String... strings) {
+            try {
+                String str;
+                URL url = new URL("보낼 jsp 경로");//보낼 jsp 주소를 ""안에 작성합니다.
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+                conn.setRequestMethod("POST");//데이터를 POST 방식으로 전송합니다.
+                OutputStreamWriter osw = new OutputStreamWriter(conn.getOutputStream());
+                sendMsg = "id="+strings[0]+"&pwd="+strings[1];//보낼 정보인데요. GET방식으로 작성합니다. ex) "id=rain483&pwd=1234";
+                //회원가입처럼 보낼 데이터가 여러 개일 경우 &로 구분하여 작성합니다.
+                osw.write(sendMsg);//OutputStreamWriter에 담아 전송합니다.
+                osw.flush();
+                //jsp와 통신이 정상적으로 되었을 때 할 코드들입니다.
+                if(conn.getResponseCode() == conn.HTTP_OK) {
+                    InputStreamReader tmp = new InputStreamReader(conn.getInputStream(), "UTF-8");
+                    BufferedReader reader = new BufferedReader(tmp);
+                    StringBuffer buffer = new StringBuffer();
+                    //jsp에서 보낸 값을 받겠죠?
+                    while ((str = reader.readLine()) != null) {
+                        buffer.append(str);
+                    }
+                    receiveMsg = buffer.toString();
+
+                } else {
+                    Log.i("통신 결과", conn.getResponseCode()+"에러");
+                    // 통신이 실패했을 때 실패한 이유를 알기 위해 로그를 찍습니다.
+                }
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            //jsp로부터 받은 리턴 값입니다.
+            return receiveMsg;
+        }
     }
 
     // 회원가입 버튼을 눌르면 나오는 다이얼로그
