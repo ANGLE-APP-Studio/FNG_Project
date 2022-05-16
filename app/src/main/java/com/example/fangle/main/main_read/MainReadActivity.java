38f390ba9c22b0ae1e5f763125afe020b4646a5f
igh2932 <76648508+igh2932@users.noreply.github.com>
2022-05-17 01:55:44 +0900

    FNG-001 배너 자동 스크롤 구현하기

--- app/src/main/java/com/example/fangle/main/main_read/MainReadActivity.java
+++ app/src/main/java/com/example/fangle/main/main_read/MainReadActivity.java
@@ -2,18 +2,34 @@ package com.example.fangle.main.main_read;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.HorizontalScrollView;
+import android.widget.ScrollView;
 
 import com.example.fangle.R;
 
 public class MainReadActivity extends AppCompatActivity {
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActionBar ab = getSupportActionBar() ;
         setContentView(R.layout.activity_main_read);
 
+        final HorizontalScrollView HorizontalScrollView = ((HorizontalScrollView) findViewById(R.id.HorizontalScrollView));
+        HorizontalScrollView.post(new Runnable() {
+            @Override
+            public void run() {
+                ObjectAnimator.ofInt(HorizontalScrollView, "scrollX", 10000).setDuration(100000).start();
+
+            }
+
+        });
     }
+
 }
