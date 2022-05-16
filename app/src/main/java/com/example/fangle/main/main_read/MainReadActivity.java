f69faa2ec28c88cd7c24f0b81bf26f910ba89d51
igh2932 <76648508+igh2932@users.noreply.github.com>
2022-05-16 22:42:31 +0900

    FNG-001 메인 상단바 구현

--- app/src/main/java/com/example/fangle/main/main_read/MainReadActivity.java
+++ app/src/main/java/com/example/fangle/main/main_read/MainReadActivity.java
@@ -1,16 +1,19 @@
 package com.example.fangle.main.main_read;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
 import com.example.fangle.R;
 
-public class Main_Read_Activity extends AppCompatActivity {
+public class MainReadActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ActionBar ab = getSupportActionBar() ;
         setContentView(R.layout.activity_main_read);
+
     }
 }
