f60ba2f758f8bc2fd9ad05c8dfd56439bbfbf21b
igh2932 <igh2932@gmail.com>
2022-05-17 16:07:03 +0900

    CWR-001 게시글 목록

---
+++ app/src/main/java/com/example/fangle/writing/writing_read/WritingReadActivity.java
@@ -0,0 +1,16 @@
+package com.example.fangle.writing.writing_read;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.fangle.R;
+
+public class WritingReadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_writing_read);
+    }
+}
