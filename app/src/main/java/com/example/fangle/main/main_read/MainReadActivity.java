0df2c411daffac33aabad0fad956b9beae46a866
igh2932 <igh2932@gmail.com>
2022-05-17 14:46:42 +0900

    FNG-001 달력

--- app/src/main/java/com/example/fangle/main/main_read/MainReadActivity.java
+++ app/src/main/java/com/example/fangle/main/main_read/MainReadActivity.java
@@ -8,19 +8,23 @@ import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.widget.CalendarView;
 import android.widget.HorizontalScrollView;
 import android.widget.ScrollView;
 
 import com.example.fangle.R;
 
 public class MainReadActivity extends AppCompatActivity {
-    
+
+    public CalendarView calendarView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActionBar ab = getSupportActionBar() ;
         setContentView(R.layout.activity_main_read);
+        calendarView = (CalendarView) findViewById(R.id.calendarView);
 
+        // 배너 뷰 타이머
         final HorizontalScrollView HorizontalScrollView = ((HorizontalScrollView) findViewById(R.id.HorizontalScrollView));
         HorizontalScrollView.post(new Runnable() {
             @Override
@@ -28,8 +32,10 @@ public class MainReadActivity extends AppCompatActivity {
                 ObjectAnimator.ofInt(HorizontalScrollView, "scrollX", 10000).setDuration(100000).start();
 
             }
-
         });
+
+
+
     }
 
 }
