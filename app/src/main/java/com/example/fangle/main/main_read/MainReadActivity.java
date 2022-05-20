2a6d92637d2bf2ee1cc1821bdec8948a13ab87d9
igh2932 <76648508+igh2932@users.noreply.github.com>
2022-05-20 22:01:46 +0900

    FNG-001 상단바 수정 및 아이콘 수정

--- app/src/main/java/com/example/fangle/main/main_read/MainReadActivity.java
+++ app/src/main/java/com/example/fangle/main/main_read/MainReadActivity.java
@@ -1,5 +1,6 @@
 package com.example.fangle.main.main_read;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
@@ -7,25 +8,36 @@ import androidx.viewpager.widget.ViewPager;
 import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.CalendarView;
 import android.widget.HorizontalScrollView;
 import android.widget.ScrollView;
+import androidx.appcompat.widget.Toolbar;
 
 import com.example.fangle.R;
 
 public class MainReadActivity extends AppCompatActivity {
 
-<<<<<<< HEAD
-=======
+    Toolbar toolbar;
     public CalendarView calendarView;
 
->>>>>>> 0df2c411daffac33aabad0fad956b9beae46a866
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_read);
+
         calendarView = (CalendarView) findViewById(R.id.calendarView);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+
+        // 홈버튼 활성화 및 홈버튼 이미지 변경
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.menu);
+
+        getSupportActionBar().setDisplayShowCustomEnabled(false);
 
         // 배너 뷰 타이머
         final HorizontalScrollView HorizontalScrollView = ((HorizontalScrollView) findViewById(R.id.HorizontalScrollView));
@@ -37,8 +49,26 @@ public class MainReadActivity extends AppCompatActivity {
             }
         });
 
+    }
 
+    // 상단 툴바 추가
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.action_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
+    // 상단 툴바 클릭 처리
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == android.R.id.home){
+
+        }
+        if (id == R.id.setting_item){
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
