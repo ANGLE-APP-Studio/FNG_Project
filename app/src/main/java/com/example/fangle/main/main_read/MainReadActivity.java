b248f9010aeb719b08fef42d91de58ab8f11416f
igh2932 <76648508+igh2932@users.noreply.github.com>
2022-05-23 00:18:06 +0900

    FNG-001 하단바 프래그먼트 구현 (커뮤니티,메인 , 프로필 을 프래그먼트로 구현 해야함)

--- app/src/main/java/com/example/fangle/main/main_read/MainReadActivity.java
+++ app/src/main/java/com/example/fangle/main/main_read/MainReadActivity.java
@@ -1,53 +1,70 @@
 package com.example.fangle.main.main_read;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.ViewPager;
 
-import android.animation.ObjectAnimator;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.CalendarView;
-import android.widget.HorizontalScrollView;
-import android.widget.ScrollView;
+import android.widget.Toast;
+
 import androidx.appcompat.widget.Toolbar;
 
+import com.example.fangle.profile.profile_read.ProfileReadFragment;
 import com.example.fangle.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainReadActivity extends AppCompatActivity {
 
     Toolbar toolbar;
+
     public CalendarView calendarView;
+
+    // 하단바 Fragment
+    ProfileReadFragment profileReadFragment;
+    MainReadFragment mainReadFragment;
+
+    // 여기에 입력
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_read);
 
-        calendarView = (CalendarView) findViewById(R.id.calendarView);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
-
         setSupportActionBar(toolbar);
+
+        //프레그면트
+        profileReadFragment = new ProfileReadFragment();
+        mainReadFragment = new MainReadFragment();
+
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, mainReadFragment).commit();
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setSelectedItemId(R.id.home_tab);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home_tab:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, mainReadFragment).commit();
+                        return true;
+                    case R.id.account_tab:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, profileReadFragment).commit();
+                        return true;
+                }
+                return false;
+            }
+        });
+
+
+
 
         // 홈버튼 활성화 및 홈버튼 이미지 변경
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.menu);
-
         getSupportActionBar().setDisplayShowCustomEnabled(false);
-
-        // 배너 뷰 타이머
-        final HorizontalScrollView HorizontalScrollView = ((HorizontalScrollView) findViewById(R.id.HorizontalScrollView));
-        HorizontalScrollView.post(new Runnable() {
-            @Override
-            public void run() {
-                ObjectAnimator.ofInt(HorizontalScrollView, "scrollX", 10000).setDuration(100000).start();
-
-            }
-        });
 
     }
 
@@ -63,11 +80,12 @@ public class MainReadActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
         if(id == android.R.id.home){
-
+            Toast.makeText(this,"메뉴", Toast.LENGTH_LONG).show();
         }
         if (id == R.id.setting_item){
-
+            Toast.makeText(this,"셋팅", Toast.LENGTH_LONG).show();
         }
+
         return super.onOptionsItemSelected(item);
     }
 
---
+++ app/src/main/java/com/example/fangle/main/main_read/MainReadFragment.java
@@ -0,0 +1,35 @@
+package com.example.fangle.main.main_read;
+
+import android.animation.ObjectAnimator;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.HorizontalScrollView;
+
+import com.example.fangle.R;
+
+public class MainReadFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_main_read, container, false);
+
+        // 배너 뷰 타이머
+        final HorizontalScrollView HorizontalScrollView = ((HorizontalScrollView) v.findViewById(R.id.HorizontalScrollView));
+        HorizontalScrollView.post(new Runnable() {
+            @Override
+            public void run() {
+                ObjectAnimator.ofInt(HorizontalScrollView, "scrollX", 10000).setDuration(100000).start();
+
+            }
+        });
+
+        return v;
+    }
+
+}
