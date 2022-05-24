8500c16020ea4308dc99ce7373e26063b79288f7
HH209-14\seoil <gkswn99@gmail.com>
2022-05-24 15:53:22 +0900

    커뮤니티

---
+++ app/src/main/java/com/example/fangle/join/JoinActivity.java
@@ -0,0 +1,16 @@
+package com.example.fangle.join;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.fangle.R;
+
+public class JoinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join);
+    }
+}
