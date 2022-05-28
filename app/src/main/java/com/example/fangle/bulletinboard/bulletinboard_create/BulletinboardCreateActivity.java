30c4c1b48ce8f66a67601afc5ec6619994e4228c
igh2932 <76648508+igh2932@users.noreply.github.com>
2022-05-29 00:21:58 +0900

    BBC-001 게시판 생성 구현 준비

---
+++ app/src/main/java/com/example/fangle/bulletinboard/bulletinboard_create/BulletinboardCreateActivity.java
@@ -0,0 +1,16 @@
+package com.example.fangle.bulletinboard.bulletinboard_create;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.fangle.R;
+
+public class BulletinboardCreateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bulletinboard_create);
+    }
+}
