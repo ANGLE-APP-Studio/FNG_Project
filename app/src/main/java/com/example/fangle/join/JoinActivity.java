2a0951260adc38ec193dd8c1ee0322b87c6337b5
HH209-14\seoil <gkswn99@gmail.com>
2022-05-31 15:24:25 +0900

    수정

--- app/src/main/java/com/example/fangle/join/JoinActivity.java
+++ app/src/main/java/com/example/fangle/join/JoinActivity.java
@@ -3,10 +3,14 @@ package com.example.fangle.join;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.fangle.R;
 
 public class JoinActivity extends AppCompatActivity {
+
+    Button btn01, btn02;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
